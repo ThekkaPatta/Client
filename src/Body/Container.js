833e3b196d1abf8334c550919cb4da163328c7f8
saugatbhattarai754 <saugatb55@gmail.com>
2021-07-26 15:50:53 +0545

    bidding work don

--- src/Body/Container.js
+++ src/Body/Container.js
@@ -40,8 +40,6 @@ class Container extends Component{
                 <Route path="/userhome" component={userhome} />
                
                 
-            </div>
-
                 
 
             </div>
